563855738f3a3f4e682e850639cf2bdfb7cdad91
Carley Jacobo <carleyj413@gmail.com>
2025-08-26 17:38:21 -0700

    homework assignment completed

---
+++ index.js
@@ -0,0 +1,26 @@
+let myhttp = require("http");
+
+let myserver = myhttp.createServer(
+    function( myrequest, myresponse) {
+        console.log( myrequest.url );
+
+        let mytext;
+        if ( myrequest.url === '/hey' ) {
+            mytext = "Well hello there...";
+        } 
+        else if (myrequest.url === '/homeworkweekone') {
+            mytext = "Yes, it works!";
+        } 
+        else if (myrequest.url === '/thirdoption') {
+            mytext = "Third works too!";
+        } else {
+            mytext = "None of those!";
+        }
+
+        myresponse.writeHead( 200, { "Content-Type": "text/plain" } );
+
+        myresponse.end( mytext + '\n');
+    }
+);
+
+myserver.listen( 8080, "127.0.0.1")
